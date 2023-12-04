4bf76f4dd24d8b46ae37dfac5dd67d040f2dd403
Eric Felner <felnerer@outlook.com>
2023-12-04 10:25:11 -0500

    added practice medium 1 problems

---
+++ lesson3_practice_problems/practice_problems_medium_1/practice_medium1_8.rb
@@ -0,0 +1,22 @@
+# Practice Problems: Medium 1-8
+
+=begin
+Method calls can take expressions as arguments. Suppose we define a method 
+called rps as follows, which follow the classic rules of rock-paper-scissors 
+game: it returns the winning fist or, in the case of a time, the fist that both
+players played.
+
+def rps(fist1, fist2)
+  if fist1 == "rock"
+    (fist2 == "paper") ? "paper" : "rock"
+  elsif fist1 == "paper"
+    (fist2 == "scissors") ? "scissors" : "paper"
+  else
+    (fist2 == "rock") ? "rock" : "scissors"
+  end
+end
+
+What is the result of the following call?
+
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+
