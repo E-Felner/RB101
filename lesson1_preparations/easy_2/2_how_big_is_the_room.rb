acf2da39002cba42e9e622fad4114c89cfa13f69
Eric Felner <felnerer@outlook.com>
2024-02-04 11:23:18 -0500

    r101 lesson 1 easy 2 problems 2&3 added more

--- lesson1_preparations/easy_2/2_how_big_is_the_room.rb
+++ lesson1_preparations/easy_2/2_how_big_is_the_room.rb
@@ -1,5 +1,17 @@
 # rb101 easy 2 set, problem 2 How big is the room?
 
+=begin
+input two integers as strings
+output float
+
+get two integers as strings from user to be length and width in meters
+  convert to integers
+multiply the two numbers to get the area in meters**2
+  convert to feet**2
+print a string with both the meters**2 and feet**2 interpolated
+=end
+
+=begin
 puts "What is the length of the room in meters?"
 length = gets.chomp
 
@@ -12,6 +24,7 @@ area_sq_ft = area_sq_meters * 10.7639
 
 puts "The area of the room is #{area_sq_meters} square meters (#{area_sq_ft} \
 square feet)."
+=end
 
 =begin
 SQMETERS_TO_SQFEET = 10.7639
@@ -29,13 +42,20 @@ puts "The area of the room is #{square_meters} " + \
      "square meters (#{square_feet} square feet)."
 =end
 
-=begin
-input two integers as strings
-output float
+SQMETERS_TO_SQFEET = 10.7639
 
-get two integers as strings from user to be length and width in meters
-  convert to integers
-multiply the two numbers to get the area in meters**2
-  convert to feet**2
-print a string with both the meters**2 and feet**2 interpolated
-=end
+puts '==> Enter the length of the room in feet: '
+length = gets.to_f
+
+puts '==> Enter the width of the room in feet: '
+width = gets.to_f
+
+square_feet = (length * width).round(2)
+
+square_inches = (square_feet * 144).round(2)
+
+square_centimeters = ((square_feet / SQMETERS_TO_SQFEET) * 10000).round(2)
+
+puts "The area of the room is #{square_feet} square feet."
+puts "The area of the room is #{square_inches} square inches."
+puts "The area of the room is #{square_centimeters} square centimeters."
--- lesson1_preparations/easy_2/3_tip_calculator.rb
+++ lesson1_preparations/easy_2/3_tip_calculator.rb
@@ -9,10 +9,8 @@ tip_percentage = gets.chomp
 tip = ((tip_percentage.to_f / 100) * bill.to_f)
 total_bill = (bill.to_f + tip)
 
-=begin
-tip = format("%.2f", tip)                       
+tip = format("%.2f", tip)
 total_bill = format("%.2f", total_bill)
-=end
 
 puts "The tip is $#{tip}"
 puts "The total is $#{total_bill}"
@@ -41,7 +39,7 @@ get the bill amount and get the tip percentage from user
   convert both to integers
 multiply bill amount and tip to get tip amount
 add tip amount to bill amount to get total amount
-
+print tip amount and total amount
 
 puts "What is the bill?"
 bill = gets.chomp.to_i
